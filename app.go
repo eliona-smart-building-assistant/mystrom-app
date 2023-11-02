5fd11b51fed13e832d0a37848ed614d021212d48
Zdeněk Devátý <zdenek.devaty@gmail.com>
2023-11-02 15:43:21 +0100

    Initial commit

---
+++ app.go
@@ -0,0 +1,113 @@
+//  This file is part of the eliona project.
+//  Copyright © 2022 LEICOM iTEC AG. All Rights Reserved.
+//  ______ _ _
+// |  ____| (_)
+// | |__  | |_  ___  _ __   __ _
+// |  __| | | |/ _ \| '_ \ / _` |
+// | |____| | | (_) | | | | (_| |
+// |______|_|_|\___/|_| |_|\__,_|
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package main
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"template/apiserver"
+	"template/apiservices"
+	"template/conf"
+	"template/eliona"
+	"time"
+
+	"github.com/eliona-smart-building-assistant/go-utils/common"
+	utilshttp "github.com/eliona-smart-building-assistant/go-utils/http"
+	"github.com/eliona-smart-building-assistant/go-utils/log"
+)
+
+var once sync.Once
+
+func collectData() {
+	configs, err := conf.GetConfigs(context.Background())
+	if err != nil {
+		log.Fatal("conf", "Couldn't read configs from DB: %v", err)
+		return
+	}
+	if len(configs) == 0 {
+		once.Do(func() {
+			log.Info("conf", "No configs in DB. Please configure the app in Eliona.")
+		})
+		return
+	}
+
+	for _, config := range configs {
+		if !conf.IsConfigEnabled(config) {
+			if conf.IsConfigActive(config) {
+				conf.SetConfigActiveState(context.Background(), config, false)
+			}
+			continue
+		}
+
+		if !conf.IsConfigActive(config) {
+			conf.SetConfigActiveState(context.Background(), config, true)
+			log.Info("conf", "Collecting initialized with Configuration %d:\n"+
+				"Enable: %t\n"+
+				"Refresh Interval: %d\n"+
+				"Request Timeout: %d\n"+
+				"Project IDs: %v\n",
+				*config.Id,
+				*config.Enable,
+				config.RefreshInterval,
+				*config.RequestTimeout,
+				*config.ProjectIDs)
+		}
+
+		common.RunOnceWithParam(func(config apiserver.Configuration) {
+			log.Info("main", "Collecting %d started.", *config.Id)
+			if err := collectResources(&config); err != nil {
+				return // Error is handled in the method itself.
+			}
+			log.Info("main", "Collecting %d finished.", *config.Id)
+
+			time.Sleep(time.Second * time.Duration(config.RefreshInterval))
+		}, config, *config.Id)
+	}
+}
+
+func collectResources(config *apiserver.Configuration) error {
+	// Do the magic here
+	return nil
+}
+
+// listenForOutputChanges listens to output attribute changes from Eliona. Delete if not needed.
+func listenForOutputChanges() {
+	for { // We want to restart listening in case something breaks.
+		outputs, err := eliona.ListenForOutputChanges()
+		if err != nil {
+			log.Error("eliona", "listening for output changes: %v", err)
+			return
+		}
+		for output := range outputs {
+			_ = output
+			// Do the output magic here.
+		}
+		time.Sleep(time.Second * 5) // Give the server a little break.
+	}
+}
+
+// listenApi starts the API server and listen for requests
+func listenApi() {
+	err := http.ListenAndServe(":"+common.Getenv("API_SERVER_PORT", "3000"), utilshttp.NewCORSEnabledHandler(
+		apiserver.NewRouter(
+			apiserver.NewConfigurationAPIController(apiservices.NewConfigurationApiService()),
+			apiserver.NewVersionAPIController(apiservices.NewVersionApiService()),
+			apiserver.NewCustomizationAPIController(apiservices.NewCustomizationApiService()),
+		)),
+	)
+	log.Fatal("main", "API server: %v", err)
+}
